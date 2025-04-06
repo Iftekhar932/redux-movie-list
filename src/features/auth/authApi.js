4f648bb7978b36dd3b952ddaa1f9d878418ec23d
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2025-04-06 10:06:51 +0600

    added interceptor.request for token setup

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -3,3 +3,19 @@ export const auth_api = axios.create({
   baseURL: "http://localhost:5000/auth",
   withCredentials: true,
 });
+
+// ! needs testing
+auth_api.interceptors.request.use((config) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    } else {
+      delete config.headers["Authorization"];
+    }
+    return config;
+  } catch (error) {
+    console.log("🚀 ~ error:", error);
+    return Promise.reject(error);
+  }
+});
